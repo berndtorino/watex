e57f16c37fd9fd16aa96a5e8cce8e8080d94a110
ninov <dev@ninov.de>
2017-01-18 12:56:41 +0100

    version 2

--- watex.user.js
+++ watex.user.js
@@ -2,7 +2,7 @@
 // @name        watex
 // @namespace   watex
 // @include     https://web.whatsapp.com/
-// @version     1
+// @version     2
 // @grant       none
 // @updateURL   https://github.com/ninov/watex/raw/master/watex.user.js
 // @downloadURL https://github.com/ninov/watex/raw/master/watex.user.js
